4bbe5b070b4319ca134d9c5e3936563231c0d10f
Heidarie <dawidogly@gmail.com>
2022-01-29 12:54:34 +0100

    return userid and role with token

--- BeginnerWebApiRC1/Models/TokenModel.cs
+++ BeginnerWebApiRC1/Models/TokenModel.cs
@@ -4,5 +4,7 @@
     {
         public string AccessToken { get; set; }
         public string RefreshToken { get; set; }
+        public string UserRole { get; set; }
+        public string UserId { get; set; }
     }
 }

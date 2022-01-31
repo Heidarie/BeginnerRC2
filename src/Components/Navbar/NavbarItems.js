c97ade15dda95813090ad9ddd018ebae69594923
Maciej Moczadło <maciek.moczadlo@gmail.com>
2022-01-31 20:10:37 +0100

    Remove not needed from navbar

--- src/Components/Navbar/NavbarItems.js
+++ src/Components/Navbar/NavbarItems.js
@@ -1,22 +1,22 @@
 export const NavbarItems = [
-  {
-    id: 1,
-    title: "Oferty Pracy",
-    url: "OfertyPracy",
-  },
-  {
-    id: 2,
-    title: "Dla Pracodawców",
-    url: "/DlaPracodawcow",
-  },
-  {
-    id: 3,
-    title: "Mapa pracy",
-    url: "MapaPracy",
-  },
-  {
-    id: 4,
-    title: "O nas",
-    url: "O-Nas",
-  },
+  // {
+  //   id: 1,
+  //   title: "Oferty Pracy",
+  //   url: "OfertyPracy",
+  // },
+  // {
+  //   id: 2,
+  //   title: "Dla Pracodawców",
+  //   url: "/DlaPracodawcow",
+  // },
+  // {
+  //   id: 3,
+  //   title: "Mapa pracy",
+  //   url: "MapaPracy",
+  // },
+  // {
+  //   id: 4,
+  //   title: "O nas",
+  //   url: "O-Nas",
+  // },
 ];

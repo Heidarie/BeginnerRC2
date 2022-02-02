6cf5388a72d4785494249311bf3c6fb3efb4eea6
Heidarie <dawidogly@gmail.com>
2022-02-02 23:54:46 +0100

    added finish date

--- BeginnerWebApiRC1/Models/Offer/ShortOfferModel.cs
+++ BeginnerWebApiRC1/Models/Offer/ShortOfferModel.cs
@@ -18,6 +18,7 @@ namespace BeginnerWebApiRC1.Models.Offer
             Id = offer.Id;
             Profession = profession.Profession1;
             Languages = offer.AdditionalData != null ? new List<string>(offer.AdditionalData.Languages.Split(";")) : null;
+            FinishDate = offer.Fd.ToLongDateString();
         }
         public int Id { get; set; }
         public string PositionName { get; set; }
@@ -26,6 +27,7 @@ namespace BeginnerWebApiRC1.Models.Offer
         public string SalaryFrom { get; set; }
         public string SalaryTo { get; set; }
         public string CreationDate { get; set; }
+        public string FinishDate { get; set; }
         public string Profession { get; set; }
         public List<string> Languages { get; set; }
     }
